306230fb258b1ca4e0ef0f5425a9ecac1e7ab0d9
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2015-08-18 16:49:19 -0400

    more lua binding stuff

--- foneOS/LuaLayout.cpp
+++ foneOS/LuaLayout.cpp
@@ -3,6 +3,13 @@
 #include "LuaCustomTypes.hpp"
 #include "LuaLayout.h"
 
+int Lua_FoneOSScreen_Draw(lua_State * _L)
+{
+	FoneOSScreen* obj = luaW_check<FoneOSScreen>(_L, 1);
+	obj->Draw();
+	return 0;
+}
+
 static luaL_Reg FoneOSContainer_table[] =
 {
 	{ NULL, NULL }
@@ -18,7 +25,13 @@ static luaL_Reg FoneOSContainer_metatable[] =
 static luaL_Reg FoneOSLabel_metatable[] =
 {
 	{ "text", luaU_getset<FoneOSLabel, FoneOSString, &FoneOSLabel::text> },
-	{ NULL, NULL}
+	{ NULL, NULL }
+};
+
+static luaL_Reg FoneOSScreen_metatable[] =
+{
+	{ "draw", Lua_FoneOSScreen_Draw },
+	{ NULL, NULL }
 };
 
 int luaopen_Layout(lua_State* L)
@@ -33,6 +46,8 @@ int luaopen_Layout(lua_State* L)
 
 	luaW_register<FoneOSLabel>(L, "FoneOSLabel", FoneOSContainer_table, FoneOSLabel_metatable);
 	luaW_extend<FoneOSLabel, FoneOSContainer>(L); // Informs the wrapper that FoneOSLabel inherits from the class FoneOSContainer
+
+	luaW_register<FoneOSScreen>(L, "FoneOSScreen", FoneOSContainer_table, FoneOSScreen_metatable);
 
 	return 1;
 }
