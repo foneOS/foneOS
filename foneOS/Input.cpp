3b60296aa1f3cace54a9247a09457d60cdd067e2
thatoddmailbox <thatoddmailbox@users.noreply.github.com>
2016-01-20 22:30:08 -0500

    stop logging every point

--- foneOS/Input.cpp
+++ foneOS/Input.cpp
@@ -19,7 +19,7 @@ FoneOSPoint Input::GetTouch()
 		return FoneOSPoint();
 	}
 	FoneOSPoint ret = _points[0];
-    Logging::LogMessage(Utils::IntToString(ret.x) + STR("-") + Utils::IntToString(ret.y) + STR("-") + Utils::IntToString(ret.z));
+	//Logging::LogMessage(Utils::IntToString(ret.x) + STR("-") + Utils::IntToString(ret.y) + STR("-") + Utils::IntToString(ret.z));
 	_points.erase(_points.begin());
 	return ret;
 }
